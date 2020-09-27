714e22c5d8694ad886f584d69356d34e527fe783
Dewalt-arch <59529711+Dewalt-arch@users.noreply.github.com>
2020-09-27 03:17:49 -0500

    Update pimpmykali.sh

--- pimpmykali.sh
+++ pimpmykali.sh
@@ -5,6 +5,13 @@
 #
 # Usage: sudo ./pimpmykali.sh  ( defaults to the menu system )  command line arguements are valid, only catching 1 arguement
 #
+
+# Revision 0.3a: 
+#   - the extraction of the impacket-0.9.19.tar.gz was leaving /opt/impacket-0.9.19 with 700 perms
+#     and an ownership of 503:root, this has been changed to ownership root:root and all files inside
+#     /opt/impacket-0.9.19 have had their permissions set to 755 after extraction of impacket-0.9.19.tar.gz
+#   - ascii art added to the menu
+#
 #   Revision 0.3: 
 #   - Added checks for already installed installations, added --force command ( --force will run all fixes/reinstalls )
 #   - fix_impacket : added both .py and .pyc files to impacket removal array
