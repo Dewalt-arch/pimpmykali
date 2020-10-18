30363b6c23b71a91ec68efd6148f0d6d899aba07
Dewalt <>
2020-10-18 14:05:33 -0500

    Update Sun Oct 18 02:05:33 PM CDT 2020

--- pimpmykali.sh
+++ pimpmykali.sh
@@ -241,7 +241,7 @@ install_sublime () {
     echo -e "\n  $greenplus installing sublime text editor"
     eval wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
     eval apt-get install apt-transport-https
-    eval echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+    eval echo "deb https://download.sublimetext.com/ apt/stable/" > /etc/apt/sources.list.d/sublime-text.list
     eval apt update
     eval apt -y install sublime-text
     }
