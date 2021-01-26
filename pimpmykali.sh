17b5089c12ddf82e80628cbe4f38075f091091e9
Dewalt <>
2021-01-26 04:05:46 -0600

    Update Tue Jan 26 04:05:46 AM CST 2021

--- pimpmykali.sh
+++ pimpmykali.sh
@@ -151,6 +151,7 @@ python-pip-curl () {
 
 fix_spike () {
     # Added 01-26-2021 Current version of spike throws error, revert to old version
+    echo -e "\n  $greenplus Fix SPIKE \n"
     echo -e "\n  $greenplus removing SPIKE...\n"
     eval apt -y --allow-change-held-packages remove spike
     wget http://old.kali.org/kali/pool/main/s/spike/spike_2.9-1kali6_amd64.deb -O /tmp/spike_2.9-1kali6_amd64.deb
