87da3288352b085bed8238e1119a834ebad1f550
Dewalt <>
2023-06-18 23:43:11 -0500

    1.7.1 added chmod +x to linpeas/winpeas

--- pimpmykali.sh
+++ pimpmykali.sh
@@ -315,7 +315,9 @@ fix_linwinpeas() {
     linpeas_arr=('linpeas.sh' 'linpeas_darwin_amd64' 'linpeas_darwin_arm64' 'linpeas_fat.sh' 'linpeas_linux_386' 'linpeas_linux_amd64' 'linpeas_linux_arm')
      for linpeas_file in ${linpeas_arr[@]}; do
        echo -e "   Downloading $linpeas_file to $dest_linpeas/$linpeas_file"
+       chmod +x $dest_linpeas/$linpeas_file 
        wget -q $releases_url/$linpeas_file -O $dest_linpeas/$linpeas_file
+       chmod +x $dest_linpeas/$linpeas_file 
      done
 
     # winpeas to /opt/winpeas
@@ -325,6 +327,7 @@ fix_linwinpeas() {
      for winpeas_file in ${winpeas_arr[@]}; do
        echo -e "  [++] Downloading $winpeas_file to $dest_winpeas/$winpeas_file"
        wget -q $releases_url/$winpeas_file -O $dest_winpeas/$winpeas_file
+       chmod +x $dest_winpeas/$winpeas_file 
      done
     }
 
